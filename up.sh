b08230c25fa1363c12759b5102acf246b0b481ed
Micah Young <micah.young@gmail.com>
2018-02-02 13:55:08 -0500

    Remove bosh/cf dependencies

--- up.sh
+++ up.sh
@@ -90,60 +90,6 @@ openstack user delete alt_demo
 #Make demo user an admin
 openstack role add --project demo --user demo admin
 
-#Bosh
-openstack quota set demo --instances 25
-openstack quota set demo --cores 25
-openstack quota set demo --volumes 20
-openstack quota set demo --ram 122880
-openstack quota set demo --networks 10
-cat > id_bosh_rsa.pub <<EOF2
-ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjiROOp2ClfiN0/9k+le/jMqHTI0/akgggCZ2hDf9aGhNFaVwdnU/yrKtCIobYv6LPX/uwQBwXUgWQ5ezlffe79RWJs7OQYEsN8aOSlqcBqfap0f2K0sQpU9jYvJuUdOw/pzpHAGo5yFlW8oCSJke/DU3LGqJkw/CVOCq1pohczVgYiBia0Un4l9CceT22bb2ZxMfy26jw0VtX4cC2UtVyfXI9xjaqbzFCJwQcIe8ECom0e7RLF0aglCSs+gwoRg/HK7NjnFPLVL0CuB4aBD+B6eLtI0LxB1ixcsnRi/UXeLFKfs+jwysUEgcN1H5pY8N/X44yNQ+OkMXZ/7PwpH/d vcap@bosh-init 
-EOF2
-openstack keypair create --public-key=id_bosh_rsa.pub bosh
-openstack security group create bosh
-openstack security group rule create bosh --protocol=tcp --dst-port=22
-openstack security group rule create bosh --protocol=tcp --dst-port=6868
-openstack security group rule create bosh --protocol=tcp --dst-port=8443
-openstack security group rule create bosh --protocol=tcp --dst-port=25555
-openstack security group rule create bosh --protocol=tcp --src-group=bosh
-openstack security group rule create bosh --protocol=udp --src-group=bosh
-openstack security group rule create bosh --protocol=icmp #debugging
-openstack floating ip create public --floating-ip-address 172.18.161.252
-openstack floating ip create public --floating-ip-address 172.18.161.253
-openstack floating ip create public --floating-ip-address 172.18.161.254
-
-#Concourse
-openstack security group rule create bosh --protocol=tcp --dst-port=8080
-
-#CloudFoundry
-openstack security group create cf
-openstack security group rule create cf --protocol=udp --dst-port=68
-openstack security group rule create cf --protocol=udp --dst-port=3457
-openstack security group rule create cf --protocol=icmp
-openstack security group rule create cf --protocol=tcp --dst-port=22
-openstack security group rule create cf --protocol=tcp --dst-port=80
-openstack security group rule create cf --protocol=tcp --dst-port=443
-openstack security group rule create cf --protocol=tcp --dst-port=4443
-openstack security group rule create cf --protocol=tcp --src-group=cf
-openstack security group rule create cf --protocol=udp --src-group=cf
-openstack security group rule create bosh --protocol=tcp --src-group=cf
-openstack security group rule create bosh --protocol=udp --src-group=cf
-
-#Create minimal flavor
-openstack flavor delete m1.small
-openstack flavor delete m1.medium
-openstack flavor delete m1.large
-openstack flavor delete m1.xlarge
-openstack flavor create m1.director --public --vcpus 1 --ram 2048 --disk 15
-openstack flavor create m1.small --public --vcpus 1 --ram 1024 --disk 10
-openstack flavor create m1.medium --public --vcpus 1 --ram 2048 --disk 10
-openstack flavor create m1.large --public --vcpus 1 --ram 8192 --disk 15
-openstack flavor create m1.xlarge --public --vcpus 1 --ram 16384 --disk 100
-openstack flavor create r3.xlarge --public --vcpus 1 --ram 16384 --disk 10 --ephemeral 32
-
-#output private net uuid
-openstack network show private -c id
-
 # required for private network routing
 echo Now run:
 echo sudo iptables -t nat -A POSTROUTING -o br-ex -j MASQUERADE
